4be9163efbbb1658a75bafa6be8578a7f29f39c4
parszywiec <bbboguszewski@gmail.com>
2018-11-06 16:06:46 +0100

    unity ads part1

---
+++ Assets/Scripts/AdsManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// bliblioteka do reklam
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour {
+
+    public void ShowRewardedAd()
+    {
+        Debug.Log("Showing Rewarded Ad");
+        // check if the advertisement is ready (rewardedVideo)
+        // if so Show(rewardedVideo)
+
+        //7min filmu
+    }
+}
